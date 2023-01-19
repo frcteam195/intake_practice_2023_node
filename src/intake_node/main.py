9512e828c64525f1dc7ddf428fe40bef9567c673
Tom <TSLourenco@WPI.edu>
2023-01-19 23:54:06 +0000

    Removed transform publisher to avoid crash.

--- src/intake_node/main.py
+++ src/intake_node/main.py
@@ -17,15 +17,12 @@ def ros_func():
 
     control_sub = BufferedROSMsgHandlerPy(Intake_Control)
     control_sub.register_for_updates("IntakeControl")
-    status_pub = rospy.Publisher(
-        name="IntakeStatus", data_class=Intake_Status, queue_size=50, tcp_nodelay=True)
+    status_pub = rospy.Publisher(name="IntakeStatus", data_class=Intake_Status, queue_size=50, tcp_nodelay=True)
 
     intakeRollerMotor = Motor(11, MotorType.TalonFX)
     intakeRollerMotor.set_defaults()
     intakeRollerMotor.set_neutral_mode(NeutralMode.Brake)
     intakeRollerMotor.apply()
-
-    intake_broadcaster = tf2_ros.TransformBroadcaster()
 
     rate = rospy.Rate(20)
 
@@ -39,13 +36,9 @@ def ros_func():
                 if control_sub.get().rollers_intake:
                     intakeRollerMotor.set(ControlMode.PERCENT_OUTPUT, 1.0, 0.0)
                 elif control_sub.get().rollers_outake:
-                    intakeRollerMotor.set(
-                        ControlMode.PERCENT_OUTPUT, -1.0, 0.0)
+                    intakeRollerMotor.set(ControlMode.PERCENT_OUTPUT, -1.0, 0.0)
                 else:
                     intakeRollerMotor.set(ControlMode.PERCENT_OUTPUT, 0.0, 0.0)
-
-        intake_broadcaster.sendTransform(get_intake_transforms(
-            intakeRollerMotor.get_sensor_position()))
 
         pubmsg = Intake_Status()
         status_pub.publish(pubmsg)
